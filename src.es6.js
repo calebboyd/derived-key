4d32b585c03f34f2c8a608beedf8c31bbb07200e
calebboyd <caleb.boyd@gmail.com>
2015-01-11 17:42:24 -0600

    code: replace complex constantTimeCompare

--- src.es6.js
+++ src.es6.js
@@ -1,13 +1,13 @@
 import { pbkdf2, randomBytes } from 'crypto';
 
-// Iterations(HEX)|Key+Salt(ENCODING)
-const SEPARATOR = '|'
+//hash formatting
+const SEPARATOR = '.'
 const ENCODING = 'ascii'
-const ENCODING_BYTE_LENGTH = 1
-const KEY_LENGTH = 32 * ENCODING_BYTE_LENGTH
-const SALT_SIZE = 16 * ENCODING_BYTE_LENGTH
-const MAX_KEY_CHARS = 1024
 const ITERATION_RADIX = 16
+
+//hash length
+const KEY_LENGTH = 32
+const SALT_SIZE = 16
 const MAX_SAFE_INTEGER = 9007199254740991
 
 /**
@@ -64,35 +64,34 @@ export function store(iterations,key,salt){
 export function hash(secret, iterations, cb){
   cb = cb || iterations
   iterations = iterations === cb ? getIterationsFromYear(getYear()) : iterations
-  salt(SALT_SIZE,(err,salt) => err && cb(err) ||
-    pbkdf2(secret, salt, iterations, KEY_LENGTH,(err,key) =>
+  salt(SALT_SIZE,(err,salt) => err ? cb(err) :
+    pbkdf2(secret, salt.toString(ENCODING), iterations, KEY_LENGTH,(err,key) =>
       cb(err || null, err ? void 0 : store(iterations,key,salt))))
 }
 
 /**
  * Compare two strings in constant time
- * taken from https://github.com/ericelliott/credential
+ * //hapijs/cryptiles
  * @param a {string}
  * @param b {string}
  * @returns {boolean}
  */
 export function constantTimeCompare(a, b) {
-  //Run Un-optimized in most runtimes
-  with ({}) {
-    // Add at least one character so that there's at least one thing to modulo over.
-    a += ' '
-    b += ' '
-    let aLen = a.length,
-      bLen = b.length,
-      match = aLen === bLen ? 1 : 0,
-      i = Math.max(aLen, bLen, MAX_KEY_CHARS)
-    while (i--) {
-      // We repeat the comparison over the strings with % so that we do not compare
-      // a number to NaN, since that has different timing that comparing two numbers.
-      match &= a.charCodeAt(i % aLen) === b.charCodeAt(i % bLen) ? 1 : 0
-    }
-    return match === 1
+
+  if (typeof a !== 'string' || typeof b !== 'string') {
+    return false
   }
+  let mismatch = (a.length === b.length ? 0 : 1)
+  if (mismatch) {
+    b = a
+  }
+
+  for(var i = 0, il = a.length; i < il; ++i) {
+    let ac = a.charCodeAt(i)
+    let bc = b.charCodeAt(i)
+    mismatch |= (ac ^ bc)
+  }
+  return mismatch === 0
 }
 
 /**
@@ -104,6 +103,6 @@ export function constantTimeCompare(a, b) {
 export function verify(secret,hash,cb){
   let salt = hash.substring(hash.length - SALT_SIZE, hash.length)
   let iterations = parseInt(hash.split(SEPARATOR)[0],ITERATION_RADIX)
-  pbkdf2(secret,salt,iterations, KEY_LENGTH, (err,key) =>
-    cb(err || null, err ? void 0 : constantTimeCompare(hash,store(iterations,key,salt))))
+  pbkdf2(secret,salt,iterations, KEY_LENGTH, (err,key) => cb(err || null, err ?
+    void 0 : constantTimeCompare(hash,store(iterations,key,salt))))
 }
