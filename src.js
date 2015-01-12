ed422d6a004b244142f80c60de8c407ac0d94ea2
calebboyd <caleb.boyd@gmail.com>
2015-01-11 18:18:01 -0600

    chore: remove blacklist arg for compile

--- src.js
+++ src.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.getYear = getYear;
 exports.getIterationsFromYear = getIterationsFromYear;
 exports.salt = salt;
@@ -9,14 +11,14 @@ var pbkdf2 = require("crypto").pbkdf2;
 var randomBytes = require("crypto").randomBytes;
 
 
-// Iterations(HEX)|Key+Salt(ENCODING)
-var SEPARATOR = "|";
+//hash formatting
+var SEPARATOR = ".";
 var ENCODING = "ascii";
-var ENCODING_BYTE_LENGTH = 1;
-var KEY_LENGTH = 32 * ENCODING_BYTE_LENGTH;
-var SALT_SIZE = 16 * ENCODING_BYTE_LENGTH;
-var MAX_KEY_CHARS = 1024;
 var ITERATION_RADIX = 16;
+
+//hash length
+var KEY_LENGTH = 32;
+var SALT_SIZE = 16;
 var MAX_SAFE_INTEGER = 9007199254740991;
 
 /**
@@ -73,7 +75,7 @@ function hash(secret, iterations, cb) {
   cb = cb || iterations;
   iterations = iterations === cb ? getIterationsFromYear(getYear()) : iterations;
   salt(SALT_SIZE, function (err, salt) {
-    return err && cb(err) || pbkdf2(secret, salt, iterations, KEY_LENGTH, function (err, key) {
+    return err ? cb(err) : pbkdf2(secret, salt.toString(ENCODING), iterations, KEY_LENGTH, function (err, key) {
       return cb(err || null, err ? void 0 : store(iterations, key, salt));
     });
   });
@@ -81,28 +83,27 @@ function hash(secret, iterations, cb) {
 
 /**
  * Compare two strings in constant time
- * taken from https://github.com/ericelliott/credential
+ * //hapijs/cryptiles
  * @param a {string}
  * @param b {string}
  * @returns {boolean}
  */
 function constantTimeCompare(a, b) {
-  //Run Un-optimized in most runtimes
-  with ({}) {
-    // Add at least one character so that there's at least one thing to modulo over.
-    a += " ";
-    b += " ";
-    var aLen = a.length,
-        bLen = b.length,
-        match = aLen === bLen ? 1 : 0,
-        i = Math.max(aLen, bLen, MAX_KEY_CHARS);
-    while (i--) {
-      // We repeat the comparison over the strings with % so that we do not compare
-      // a number to NaN, since that has different timing that comparing two numbers.
-      match &= a.charCodeAt(i % aLen) === b.charCodeAt(i % bLen) ? 1 : 0;
-    }
-    return match === 1;
+  if (typeof a !== "string" || typeof b !== "string") {
+    return false;
   }
+  var mismatch = a.length === b.length ? 0 : 1;
+  if (mismatch) {
+    b = a;
+  }
+
+  for (var i = 0,
+      il = a.length; i < il; ++i) {
+    var ac = a.charCodeAt(i);
+    var bc = b.charCodeAt(i);
+    mismatch |= ac ^ bc;
+  }
+  return mismatch === 0;
 }
 
 /**
